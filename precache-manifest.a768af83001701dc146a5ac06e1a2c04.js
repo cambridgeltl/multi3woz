a330d6c779ff10deffc32daafc611827abc0e5ba
Songbo Hu <sh2091@cam.ac.uk>
2023-07-13 05:52:04 +0800

    Updates

---
+++ precache-manifest.a768af83001701dc146a5ac06e1a2c04.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde65a2a6773af6e3795b44f0a39bd88",
+    "url": "/multi3woz/index.html"
+  },
+  {
+    "revision": "21a0928c47d79022b3e0",
+    "url": "/multi3woz/static/css/2.bb57abf7.chunk.css"
+  },
+  {
+    "revision": "62e3cd782729746e68e8",
+    "url": "/multi3woz/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "21a0928c47d79022b3e0",
+    "url": "/multi3woz/static/js/2.bb771ac3.chunk.js"
+  },
+  {
+    "revision": "62e3cd782729746e68e8",
+    "url": "/multi3woz/static/js/main.ef2f421a.chunk.js"
+  },
+  {
+    "revision": "73ad9c6a39e49317f8cf",
+    "url": "/multi3woz/static/js/runtime-main.d97010cd.js"
+  },
+  {
+    "revision": "b893c1d51dd6316f841f454d4b5d1238",
+    "url": "/multi3woz/static/media/camLogo.3d5a5c79.png"
+  },
+  {
+    "revision": "30d8c76c93bc2bde77b386781ecd3a3e",
+    "url": "/multi3woz/static/media/index.30d8c76c.less"
+  },
+  {
+    "revision": "babdea6850e3d3ab2297e32a7dbb68b9",
+    "url": "/multi3woz/static/media/ltl_logo2.babdea68.svg"
+  }
+]);
